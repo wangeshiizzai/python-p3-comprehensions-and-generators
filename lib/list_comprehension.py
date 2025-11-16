12f3e11101c5a7af5b384e4391431af9b5c4f7a7
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-11-16 14:58:31 +0300

    completed first function

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return num for num in num_list % 2 == 0
+
+print (return_evens([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
     pass
